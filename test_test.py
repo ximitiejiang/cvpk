b24da2a17bce597ec8863e539a655c1c7c3c3a49
ximitiejiang <ximitiejiang@163.com>
2018-11-16 14:38:47 +0800

    update

--- test_test.py
+++ test_test.py
@@ -5,20 +5,24 @@ Created on Thu Nov 15 15:16:51 2018
 
 @author: ubuntu
 """
-
+from model.vgg16 import VGG16
+from model.Pretrainedmodels import pretrained_models
 from torch import nn
 import torch
-class Net(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.param1 = nn.Parameter(torch.randn(3,3))
-        
-        self.submodel1 = nn.Linear(3,4)
-        
-    def forward(self, input):
-        x = self.param1.input
-        x = self.submodel1(x)
-        return x
+
     
-net = Net()
-net
+
+
+
+inputs = torch.randn(1,3,224,224)
+labels = torch.tensor([[1,0]])
+model = pretrained_models('vgg16', num_classes=2)
+criterion = torch.nn.CrossEntropyLoss()    
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        
+
+optimizer.zero_grad()
+outputs = model(imgs)            
+loss = criterion(outputs, labels)     # 这一步有可能很复杂的扩展
+loss.backward()                     
+optimizer.step() 
---
+++ utils/init.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 16 09:27:12 2018
+
+@author: suliang
+"""
+
+def fc_reset_parameters(weight, bias):
+    '''该代码是在linear class层中的子函数，对该模型的参数w,b进行初始化
+    为了便于调试，去掉了self.
+    输入：weight，bias 均为tensor
+    输出：直接修改weight，bias的值
+    
+    公式：
+    
+    '''
+    stdv = 1. / math.sqrt(weight.size(1))
+    weight.data.uniform_(-stdv, stdv)
+    if bias is not None:
+        bias.data.uniform_(-stdv, stdv)
+        
+        
+def conv_reset_parameters(in_channels, kernel_size, weight, bias):
+    '''该代码是在_ConvNd class中的子函数，对该模型的参数w,b进行初始化
+    为了便于调试，去掉了self.
+    输入：weight，bias 均为tensor
+    输出：直接修改weight，bias的值
+    
+    公式：
+    
+    '''
+    n = in_channels
+    for k in kernel_size:
+        n *= k
+    stdv = 1. / math.sqrt(n)
+    weight.data.uniform_(-stdv, stdv)
+    if bias is not None:
+        bias.data.uniform_(-stdv, stdv)
+
+
+def other_init_way():
+    from torch import nn
+    nn.init.uniform()
+
+
+if __name__ == '__main__':
+    import torch
+    weight= torch.zeros(3,3)
+    bias = torch.zeros(1,1)
+    
+    fc_reset_parameters(weight, bias)
+    
+    
